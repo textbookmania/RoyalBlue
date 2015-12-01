34628a43fc786476183ab6a016601c51f900029a
Sy Morikawa <sy@hawaii.edu>
2015-12-01 08:40:27 -1000

    init sell offers

--- app/server/seeds/SellOffer.js
+++ app/server/seeds/SellOffer.js
@@ -3,8 +3,9 @@
  * @type {*[]}
  */
 var sellOfferSeeds = [
-  {isbn: "1", condition: "good",offer: 1, owner: "1"},
-  {isbn: "1", condition: "poor",offer: 2, owner: "2"}
+  {isbn: "9780470509470", condition: "good",offer: 1, owner: "1"},
+  {isbn: "9780073383095", condition: "poor",offer: 2, owner: "2"},
+  {isbn: "9780321776402", condition: "poor",offer: 5, owner: "2"}
 ];
 
 /**
