ef65568666b3f477c3760c7dec3ea3b979364bc6
Sy Morikawa <sy@hawaii.edu>
2015-12-01 08:39:43 -1000

    added initial buy offers

--- app/server/seeds/BuyOffer.js
+++ app/server/seeds/BuyOffer.js
@@ -3,8 +3,9 @@
  * @type {*[]}
  */
 var buyOfferSeeds = [
-  {isbn: "3", condition: "good",offer: 1,owner:"0"},
-  {isbn: "2", condition: "poor",offer: 2,owner:"0"}
+    {isbn: "9780470509470", condition: "good",offer: 1,owner:"0"},
+  {isbn: "9780073383095", condition: "poor",offer: 2,owner:"0"},
+  {isbn: "9780321776402", condition: "poor",offer: 5, owner: "2"}
 ];
 
 
